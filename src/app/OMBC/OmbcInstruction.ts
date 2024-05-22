e0a81270bbb562f97b8117591c63ab933862a3ca
Royk8 <roy.maestre@udea.edu.co>
2024-05-23 00:54:08 +0200

    OMBC files documented

--- src/app/OMBC/OmbcInstruction.ts
+++ src/app/OMBC/OmbcInstruction.ts
@@ -3,15 +3,39 @@ import { ID } from "@schemas";
 import { Uuid } from "../services/Uuid";
 import { Timestamp, validateTimestamp } from "../common";
 
+/**
+ * Constructor parameters for the OmbcInstruction class.
+ */
 interface ConstructorParameters {
+    /**
+     * ID of this message.
+     */
     message_id?: ID;
+    /**
+     * ID of the instruction. Must be unique in the scope of the Resource Manager, for at least the duration of the session between Resource Manager and CEM.
+     */
     id: ID;
+    /**
+     * Indicates the moment the execution of the instruction shall start. When the specified execution time is in the past, execution must start as soon as possible.
+     */
     execution_time: Timestamp;
+    /**
+     * ID of the OMBC.OperationMode that should be activated.
+     */
     operation_mode_id: ID;
+    /**
+     * The number indicates the factor with which the OMBC.OperationMode should be configured. The factor should be greater than or equal to 0 and less than or equal to 1.
+     */
     operation_mode_factor: number;
+    /**
+     * Indicates if this is an instruction during an abnormal condition.
+     */
     abnormal_condition: boolean;
 }
 
+/**
+ * Class representing an OMBC Instruction.
+ */
 export class OmbcInstruction implements OMBC_Instruction {
     message_type: "OMBC.Instruction";
     message_id: ID;
@@ -21,6 +45,17 @@ export class OmbcInstruction implements OMBC_Instruction {
     operation_mode_factor: number;
     abnormal_condition: boolean;
 
+    /**
+     * Constructs a new instance of the OmbcInstruction class.
+     * 
+     * @param {ConstructorParameters} constructorParameters - The parameters for the constructor.
+     * @param {ID} [constructorParameters.message_id] - ID of this message.
+     * @param {ID} constructorParameters.id - ID of the instruction.
+     * @param {Timestamp} constructorParameters.execution_time - Indicates the moment the execution of the instruction shall start.
+     * @param {ID} constructorParameters.operation_mode_id - ID of the OMBC.OperationMode that should be activated.
+     * @param {number} constructorParameters.operation_mode_factor - The factor with which the OMBC.OperationMode should be configured.
+     * @param {boolean} constructorParameters.abnormal_condition - Indicates if this is an instruction during an abnormal condition.
+     */
     constructor({ message_id, id, execution_time, operation_mode_id, operation_mode_factor, abnormal_condition }: ConstructorParameters) {
         validateTimestamp(execution_time);
         if (operation_mode_factor < 0 || operation_mode_factor > 1) {
@@ -35,6 +70,4 @@ export class OmbcInstruction implements OMBC_Instruction {
         this.operation_mode_factor = operation_mode_factor;
         this.abnormal_condition = abnormal_condition;
     }
-
-
 }
--- src/app/OMBC/OmbcStatus.ts
+++ src/app/OMBC/OmbcStatus.ts
@@ -3,14 +3,38 @@ import { ID } from "@schemas";
 import { Uuid } from "../services/Uuid";
 import { Timestamp, validateTimestamp } from "../common";
 
+/**
+ * Constructor parameters for the OmbcStatus class.
+ */
 interface ConstructorParameters {
+    /**
+     * ID of this message.
+     */
     message_id?: ID;
+    /**
+     * ID of the active OMBC.OperationMode.
+     */
     active_operation_mode_id: ID;
+    /**
+     * The number indicates the factor with which the OMBC.OperationMode should be configured.
+     * The factor should be greater than or equal to 0 and less or equal to 1.
+     */
     operation_mode_factor: number;
+    /**
+     * ID of the OMBC.OperationMode that was previously active.
+     * This value shall always be provided, unless the active OMBC.OperationMode is the first OMBC.OperationMode the Resource Manager is aware of.
+     */
     previous_operation_mode_id?: ID;
+    /**
+     * Time at which the transition from the previous OMBC.OperationMode to the active OMBC.OperationMode was initiated.
+     * This value shall always be provided, unless the active OMBC.OperationMode is the first OMBC.OperationMode the Resource Manager is aware of.
+     */
     transition_timestamp?: Timestamp;
 }
 
+/**
+ * Class representing an OMBC Status.
+ */
 export class OmbcStatus implements OMBC_Status {
     message_type: "OMBC.Status";
     message_id: ID;
@@ -19,12 +43,30 @@ export class OmbcStatus implements OMBC_Status {
     previous_operation_mode_id?: ID;
     transition_timestamp?: Timestamp;
 
+    /**
+     * Constructs a new instance of the OmbcStatus class.
+     * 
+     * @param {ConstructorParameters} constructorParameters - The parameters for the constructor.
+     * @param {ID} [constructorParameters.message_id] - ID of this message.
+     * @param {ID} constructorParameters.active_operation_mode_id - ID of the active OMBC.OperationMode.
+     * @param {number} constructorParameters.operation_mode_factor - The factor with which the OMBC.OperationMode should be configured.
+     * @param {ID} [constructorParameters.previous_operation_mode_id] - ID of the previously active OMBC.OperationMode.
+     * @param {Timestamp} [constructorParameters.transition_timestamp] - Time of transition from the previous to the active OMBC.OperationMode.
+     */
+    constructor({
+        message_id,
+        active_operation_mode_id,
+        operation_mode_factor,
+        previous_operation_mode_id,
+        transition_timestamp,
+    }: ConstructorParameters) {
 
-    constructor({ message_id, active_operation_mode_id, operation_mode_factor, previous_operation_mode_id, transition_timestamp }: ConstructorParameters) {
+        // Validate transition timestamp if previous operation mode ID is provided
+        if (previous_operation_mode_id) {
+            validateTimestamp(transition_timestamp);
+        }
 
-        if(previous_operation_mode_id)
-            validateTimestamp(transition_timestamp);        
-
+        // Validate the operation mode factor
         if (operation_mode_factor < 0 || operation_mode_factor > 1) {
             throw new Error("operation_mode_factor must be in the range [0, 1]");
         }
