1013e7ad5ad4e0b60d497e492c10615429c4f788
Royk8 <roy.maestre@udea.edu.co>
2024-05-23 13:53:51 +0200

    Readme Updated

--- src/app/common/NumberRange.ts
+++ src/app/common/NumberRange.ts
@@ -26,23 +26,11 @@ export class NumberRange implements GenNumberRange {
      * @param {number} params.end_of_range - The number that defines the end of the range.
      */
     constructor({ start_of_range, end_of_range }: ConstructorParameters) {
-        this.Validate({ start_of_range, end_of_range });
-
-        this.start_of_range = start_of_range;
-        this.end_of_range = end_of_range;
-    }
-
-    /**
-     * Validates the range parameters.
-     * 
-     * @param {ConstructorParameters} params - The parameters to validate.
-     * @param {number} params.start_of_range - The number that defines the start of the range.
-     * @param {number} params.end_of_range - The number that defines the end of the range.
-     * @throws {Error} If start_of_range is greater than end_of_range.
-     */
-    public Validate({ start_of_range, end_of_range }: ConstructorParameters): void {
         if (start_of_range > end_of_range) {
             throw new Error("start_of_range must be less than or equal to end_of_range");
         }
+
+        this.start_of_range = start_of_range;
+        this.end_of_range = end_of_range;
     }
 }
